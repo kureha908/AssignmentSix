25b6a1e1db573df99e43cd12b2cde1d771b41ce8
Kureha <kureha@hawaii.edu>
2019-02-28 09:35:36 -1000

    Complete Assignment #6

--- AssignmentSix/ViewController.swift
+++ AssignmentSix/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var displayTextLabel: UILabel!
+  
+    //Making the string of the text that has no character
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            self.displayTextLabel.text=""
     }
-
-
+    
+    //Created the IBAction
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        //2.Create Local variable of type String to hold the user input text
+        var userText : String!
+        //3.Set user text to the current text in the text field or user input text
+        userText = self.textField.text
+        //4.Set the displaytextlabel text to the user input text
+        self.displayTextLabel.text=userText
+        
+        
+        
+    }
 }
 
